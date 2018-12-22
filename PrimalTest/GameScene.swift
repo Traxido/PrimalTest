b05762e37236cd7845cf8e704805f0836cf75585
Traxido <andrew.sheron@gmail.com>
2018-12-22 00:40:49 -0500

    Random Movements from Nodes

--- PrimalTest/GameScene.swift
+++ PrimalTest/GameScene.swift
@@ -11,76 +11,114 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var randomTimer:Timer? = nil
+    
+    var man = SKSpriteNode(imageNamed: "man")
+    var man2 = SKSpriteNode(imageNamed: "man")
+    var man3 = SKSpriteNode(imageNamed: "man")
+    var manRunning:[SKTexture] = []
+    
+    var men:[SKSpriteNode] = []
+
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        self.backgroundColor = UIColor(red:0.86, green:0.80, blue:0.72, alpha:1.0)
+        men = [man]
+        for i in 0...men.count-1 {
+        buildMan(sprite: men[i])
         }
+        man2.position = CGPoint(x: self.frame.width / 3, y: self.frame.height / 3)
+        man3.position = CGPoint(x: self.frame.width / 1.5, y: self.frame.height / 1.5)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        self.randomTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(doSomethingRandom), userInfo: nil, repeats: true)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    @objc func doSomethingRandom() {
+        let rand = Int(arc4random_uniform(UInt32(men.count)))
+        let movementX = Int(arc4random_uniform(UInt32(self.frame.width-50)))
+        let movementY = Int(arc4random_uniform(UInt32(self.frame.height-50)))
+        let chosenSprite = men[rand]
+        moveman(location: CGPoint(x: movementX, y: movementY), sprite: chosenSprite)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        guard let touch = touches.first else {
+            return
+        }
+        let touchLocation = touch.location(in: self)
+        
+        //moveman(location: touchLocation)
+        
+        createPerson(sprite: SKSpriteNode.init(imageNamed: "man"))
+        let manToAdd = men[men.count-1]
+        manToAdd.size = CGSize(width: 18, height: 30)
+        manToAdd.position = touchLocation
+        self.addChild(manToAdd)
+        
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    
+    
+    func buildMan(sprite: SKSpriteNode) {
+        let manAnimatedAtlas = SKTextureAtlas(named: "man1")
+        var walkFrames: [SKTexture] = []
+        
+        let numImages = manAnimatedAtlas.textureNames.count
+        print(numImages)
+        for i in 0...numImages-1 {
+            let manTextureName = "run\(i)"
+            walkFrames.append(manAnimatedAtlas.textureNamed(manTextureName))
+        }
+        manRunning = walkFrames
+        sprite.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
+        sprite.size = CGSize(width: 18, height: 30)
+        self.addChild(sprite)
     }
+    
+    func animateMan(sprite: SKSpriteNode) {
+        sprite.run(SKAction.repeatForever(
+            SKAction.animate(with: manRunning,
+                             timePerFrame: 0.2,
+                             resize: false,
+                             restore: true)),
+                withKey: "manRunning")
+    }
+    
+    func moveman(location: CGPoint, sprite: SKSpriteNode) {
+        var multiplierForDirection: CGFloat
+        let manSpeed = frame.size.width / 5.0
+        let moveDifference = CGPoint(x: location.x - sprite.position.x, y: location.y - sprite.position.y)
+        let distanceToMove = sqrt(moveDifference.x * moveDifference.x + moveDifference.y * moveDifference.y)
+        let moveDuration = distanceToMove / manSpeed
+        if moveDifference.x < 0 {
+            multiplierForDirection = -1.0
+        } else {
+            multiplierForDirection = 1.0
+        }
+        sprite.xScale = abs(sprite.xScale) * multiplierForDirection
+        if sprite.action(forKey: "walkingInPlaceman") == nil {
+            // if legs are not moving, start them
+            animateMan(sprite: sprite)
+        }
+        let moveAction = SKAction.move(to: location, duration:(TimeInterval(moveDuration)))
+        let doneAction = SKAction.run({ [weak self] in
+            self!.stopAnimating(sprite: sprite)
+        })
+        let moveActionWithDone = SKAction.sequence([moveAction, doneAction])
+        sprite.run(moveActionWithDone, withKey:"manMoving")
+    }
+    
+    func createPerson(sprite: SKSpriteNode) {
+        var newPerson = SKSpriteNode()
+        newPerson = sprite
+        men.append(newPerson)
+    }
+    
+    func stopAnimating(sprite: SKSpriteNode) {
+        sprite.removeAllActions()
+    }
+    
     
     
     override func update(_ currentTime: TimeInterval) {
